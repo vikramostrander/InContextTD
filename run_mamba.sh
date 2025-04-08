67fda7cc1f270ea358bf5413eb0793116a5fa9e6
vikramostrander <vikram.ostrander@gmail.com>
2025-04-08 11:56:16 -0400

    updated experiemnts used in the paper

--- run_mamba.sh
+++ run_mamba.sh
@@ -15,50 +15,34 @@ python3 demo.py -l=3 -model mamba tf s4 -path logs/demo/demo_mamba_l3/seed_0/mod
 ####################################################### boyan chain ####################################################### 
 
 # mamba experiments
-python3 main.py -config=boyan --representable -model=mamba -l=1 --suffix=bc_mamba_l1
+python3 main.py -config=boyan --representable -model=mamba -l=1 --suffix=bc_mamba_l1 ;
 python3 main.py -config=boyan --representable -model=mamba -l=3 --mode=auto --suffix=bc_mamba_l3
-python3 main.py -config=boyan --representable -model=mamba -l=3 --mode=sequential --suffix=bc_mamba_l3_seq
 
-# nonlinear tf experiments
-python3 main.py -config=boyan --representable -model=tf -l=1 --activation=softmax --suffix=bc_tf_l1
+# tf experiments
+python3 main.py -config=boyan --representable -model=tf -l=1 --activation=softmax --suffix=bc_tf_l1 ;
 python3 main.py -config=boyan --representable -model=tf -l=3 --activation=softmax --mode=auto --suffix=bc_tf_l3
-python3 main.py -config=boyan --representable -model=tf -l=3 --activation=softmax --mode=sequential --suffix=bc_tf_l3_seq
 
 # s4 experiments
-python3 main.py -config=boyan --representable -model=s4 -l=1 --suffix=bc_s4_l1
+python3 main.py -config=boyan --representable -model=s4 -l=1 --suffix=bc_s4_l1 ;
 python3 main.py -config=boyan --representable -model=s4 -l=3 --mode=auto --suffix=bc_s4_l3
-python3 main.py -config=boyan --representable -model=s4 -l=3 --mode=sequential --suffix=bc_s4_l3_seq
 
 ####################################################### cart pole ####################################################### 
 
 # mamba experiments
-python3 main.py -config=cartpole --representable -model=mamba -l=1 --suffix=cp_mamba_l1
+python3 main.py -config=cartpole --representable -model=mamba -l=1 --suffix=cp_mamba_l1 ;
 python3 main.py -config=cartpole --representable -model=mamba -l=3 --mode=auto --suffix=cp_mamba_l3
-python3 main.py -config=cartpole --representable -model=mamba -l=3 --mode=sequential --suffix=cp_mamba_l3_seq
 
-# nonlinear tf experiments
-python3 main.py -config=cartpole --representable -model=tf -l=1 --activation=softmax --suffix=cp_tf_l1
+# tf experiments
+python3 main.py -config=cartpole --representable -model=tf -l=1 --activation=softmax --suffix=cp_tf_l1 ;
 python3 main.py -config=cartpole --representable -model=tf -l=3 --activation=softmax --mode=auto --suffix=cp_tf_l3
-python3 main.py -config=cartpole --representable -model=tf -l=3 --activation=softmax --mode=sequential --suffix=cp_tf_seq
 
-# s4 experiments
-python3 main.py -config=cartpole --representable -model=s4 -l=1 --suffix=cp_s4_l1
-python3 main.py -config=cartpole --representable -model=s4 -l=3 --mode=auto --suffix=cp_s4_l3
-python3 main.py -config=cartpole --representable -model=s4 -l=3 --mode=sequential --suffix=cp_s4_l3_seq
 
 ####################################################### mountain car ####################################################### 
 
 # mamba experiments
-python3 main.py -config=mountaincar --representable -model=mamba -l=1 --suffix=mc_mamba_l1
+python3 main.py -config=mountaincar --representable -model=mamba -l=1 --suffix=mc_mamba_l1 ;
 python3 main.py -config=mountaincar --representable -model=mamba -l=3 --mode=auto --suffix=mc_mamba_l3
-python3 main.py -config=mountaincar --representable -model=mamba -l=3 --mode=sequential --suffix=mc_mamba_l3_seq
 
-# nonlinear tf experiments
-python3 main.py -config=mountaincar --representable -model=tf -l=1 --activation=softmax --suffix=mc_tf_l1
+# tf experiments
+python3 main.py -config=mountaincar --representable -model=tf -l=1 --activation=softmax --suffix=mc_tf_l1 ;
 python3 main.py -config=mountaincar --representable -model=tf -l=3 --activation=softmax --mode=auto --suffix=mc_tf_l3
-python3 main.py -config=mountaincar --representable -model=tf -l=3 --activation=softmax --mode=sequential --suffix=mc_tf_l3_seq
-
-# s4 experiments
-python3 main.py -config=mountaincar --representable -model=s4 -l=1 --suffix=mc_s4_l1
-python3 main.py -config=mountaincar --representable -model=s4 -l=3 --mode=auto --suffix=mc_s4_l3
-python3 main.py -config=mountaincar --representable -model=s4 -l=3 --mode=sequential --suffix=mc_s4_l3_seq
